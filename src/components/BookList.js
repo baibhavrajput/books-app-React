7d7fb9ba77433209c0aa4ebca5fe3f7b13270cb9
baibhavrajput <baibhavrajputt@gmail.com>
2023-01-14 00:52:16 +0530

    initial commit

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,8 +1,11 @@
+import BookShow from "./BookShow";
 
-function BookList() {
-    return (
-        <div>Book List</div>
-    );
+function BookList({ books, onDelete }) {
+  const renderedBooks = books.map((book) => {
+    return <BookShow key={book.id} book={book} onDelete={onDelete} />;
+  });
+
+  return <div className="book-list">{renderedBooks}</div>;
 }
 
 export default BookList;
